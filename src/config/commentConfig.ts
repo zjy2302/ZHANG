e08ebfbb2ed5ff6e3194599d1b2aac41ba2bde15
ZJY <15732733901@163.com>
2026-09-11 09:45:45 +0000

    enable giscus comments

--- src/config/commentConfig.ts
+++ src/config/commentConfig.ts
@@ -2,7 +2,7 @@ import type { CommentConfig } from "../types/commentConfig";
 
 export const commentConfig: CommentConfig = {
 	// 评论系统类型: none, twikoo, waline, giscus, disqus, artalk，默认为none，即不启用评论系统
-	type: "none",
+	type: "giscus",
 
 	//twikoo评论系统配置
 	twikoo: {
@@ -53,29 +53,18 @@ export const commentConfig: CommentConfig = {
 
 	//giscus评论系统配置
 	giscus: {
-		// 设置 Giscus 评论系统仓库
-		repo: "CuteLeaf/Firefly",
-		// 设置 Giscus 评论系统仓库ID
-		repoId: "R_kgD2gfdFGd",
-		// 设置 Giscus 评论系统分类
-		category: "General",
-		// 获取 Giscus 评论系统分类ID
-		categoryId: "DIC_kwDOKy9HOc4CegmW",
-		// 获取 Giscus 评论系统映射方式
-		mapping: "title",
-		// 获取 Giscus 评论系统严格模式
-		strict: "0",
-		// 获取 Giscus 评论系统反应功能
-		reactionsEnabled: "1",
-		// 获取 Giscus 评论系统元数据功能
-		emitMetadata: "1",
-		// 获取 Giscus 评论系统输入位置
-		inputPosition: "top",
-		// 获取 Giscus 评论系统语言
-		lang: "zh-CN",
-		// 获取 Giscus 评论系统加载方式
-		loading: "lazy",
-	},
+	repo: "zjy2302/ZHANG",
+	repoId: "R_kgDOUVF15Q",
+	category: "Announcements",
+	categoryId: "DIC_kwDOUVF15c4DFXf5",
+	mapping: "pathname",
+	strict: "0",
+	reactionsEnabled: "1",
+	emitMetadata: "0",
+	inputPosition: "top",
+	lang: "zh-CN",
+	loading: "lazy",
+},
 
 	//disqus评论系统配置
 	disqus: {
